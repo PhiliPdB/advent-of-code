e489725da8fef65f5e2f91b54ae1b7db5856819c
PhiliPdB <phlpdbrn@gmail.com>
2021-12-01 10:09:03 +0100

    Day 1 of 2021

---
+++ 2021/day1/src/main.rs
@@ -0,0 +1,24 @@
+
+
+fn main() {
+    let input: Vec<_> = include_str!("../input.txt")
+        .split('\n')
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect();
+
+
+    const WINDOW_SIZE: usize = 3;
+
+    let mut increases = 0;
+    let mut last_measurement: i32 = input[0..WINDOW_SIZE].iter().sum();
+    for i in WINDOW_SIZE..input.len() {
+        let new_measurement = last_measurement - input[i - WINDOW_SIZE] + input[i];
+        if new_measurement > last_measurement {
+            increases += 1;
+        }
+        last_measurement = new_measurement;
+    }
+
+    println!("Measurement increases: {}", increases);
+
+}
