3ba205af40934768f75b6e77fff46a558f7cc1a1
PhiliPdB <phlpdbrn@gmail.com>
2021-12-02 09:24:23 +0100

    Day 2 part 1

---
+++ 2021/day02/src/main.rs
@@ -0,0 +1,48 @@
+use std::str::FromStr;
+
+
+enum Direction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+impl FromStr for Direction {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split(' ').collect();
+
+        if let Ok(magnitude) = parts[1].parse::<i32>() {
+            match parts[0] {
+                "forward" => Ok(Direction::Forward(magnitude)),
+                "down"    => Ok(Direction::Down(magnitude)),
+                "up"      => Ok(Direction::Up(magnitude)),
+                _         => Err("Invalid direction")
+            }
+        } else {
+            Err("Can't parse magnitude")
+        }
+    }
+}
+
+
+fn main() {
+    let input: Vec<_> = include_str!("../input.txt")
+        .lines()
+        .map(|s| Direction::from_str(s).unwrap())
+        .collect();
+
+    let mut forward = 0;
+    let mut depth = 0;
+
+    for direction in input {
+        match direction {
+            Direction::Forward(x) => forward += x,
+            Direction::Down(x)    => depth += x,
+            Direction::Up(x)      => depth -= x,
+        }
+    }
+
+    println!("Answer: {}", forward * depth);
+}
