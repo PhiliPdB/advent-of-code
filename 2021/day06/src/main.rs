196399a6893b7ffac8fd644463adba0c6c37b1d0
PhiliPdB <phlpdbrn@gmail.com>
2021-12-06 09:15:56 +0100

    Improve algorithm for day 6

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,40 +1,36 @@
+use std::mem;
+
 const TOTAL_DAYS: i32 = 256;
 
 fn main() {
     let input: Vec<_> = include_str!("../input.txt")
         .split(',')
-        .map(|s| s.parse::<i32>().unwrap())
+        .map(|s| s.parse::<usize>().unwrap())
         .collect();
 
-    // Merge fish with the same day together
-    let mut fish: Vec<_> = (0..6)
-        .filter_map(|day| {
-            let total_fish = input.iter().filter(|&&f| f == day).count();
-            if total_fish > 0 {
-                Some((total_fish as u64, day as i8))
-            } else {
-                None
-            }
-        })
-        .collect();
+    let mut current_fish = [0_u64; 9];
+    // Fill current fish array
+    for f in input {
+        current_fish[f] += 1;
+    }
+    // Create temporary storage for the next iteration
+    let mut next_fish = [0_u64; 9];
 
     for _current_day in 0..TOTAL_DAYS {
-        let mut new_fish = 0;
-        fish = fish.into_iter()
-            .map(|(f, day)| {
-                if day > 0 {
-                    (f, day - 1)
-                } else {
-                    new_fish += f;
-
-                    (f, 6)
-                }
-            })
-            .collect();
+        // Let all fish decrease by 1 day.
+        for day in 0..8 {
+            next_fish[day] = current_fish[day + 1];
+        }
 
         // Create new fish
-        fish.push((new_fish, 8));
+        next_fish[8] = current_fish[0];
+
+        // Reset fishes that reached day 0
+        next_fish[6] += current_fish[0];
+
+        // Push next_fish to current_fish by swapping the memory
+        mem::swap(&mut current_fish, &mut next_fish);
     }
 
-    println!("Total fish: {:}", fish.iter().map(|(f, _)| f).sum::<u64>());
+    println!("Total fish: {}", current_fish.iter().sum::<u64>());
 }
