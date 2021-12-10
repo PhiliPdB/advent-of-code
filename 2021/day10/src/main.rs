a52c226cfdadbde18075dc519959300ceb7649ec
PhiliPdB <phlpdbrn@gmail.com>
2021-12-10 07:31:15 +0100

    Day 10

---
+++ 2021/day10/src/main.rs
@@ -0,0 +1,79 @@
+
+fn bracket_matches(opening: char, closing: char) -> bool {
+    match opening {
+        '(' => closing == ')',
+        '[' => closing == ']',
+        '{' => closing == '}',
+        '<' => closing == '>',
+        _   => unreachable!(),
+    }
+}
+
+fn incorrect_bracket_score(bracket: char) -> i32 {
+    match bracket {
+        ')' =>     3,
+        ']' =>    57,
+        '}' =>  1197,
+        '>' => 25137,
+        _   => unreachable!(),
+    }
+}
+
+fn insert_bracket_score(bracket: char) -> u64 {
+    match bracket {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _   => unreachable!(),
+    }
+}
+
+
+fn parse_line(line: &str) -> (i32, Vec<char>) {
+    let mut stack = Vec::with_capacity(line.len());
+
+    for c in line.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => stack.push(c),
+            ')' | ']' | '}' | '>' => {
+                let last_item = stack.pop().unwrap();
+                if !bracket_matches(last_item, c) {
+                    return (incorrect_bracket_score(c), stack);
+                }
+            },
+            _ => panic!("Invalid bracket"),
+        }
+    }
+
+    return (0, stack);
+}
+
+fn main() {
+    let input: Vec<_> = include_str!("../input.txt")
+        .lines()
+        .map(|l| parse_line(l))
+        .collect();
+
+    // Part 1
+    let score: i32 = input.iter()
+        .map(|(s, _)| s)
+        .sum();
+    println!("Invalid syntax error score: {:#10}", score);
+
+
+    // Part 2
+    let mut fix_missing_scores: Vec<_> = input.iter()
+        // Only fix incomplete sequences and ignore the invalid ones.
+        .filter_map(|(s, remaining)| (*s == 0).then(|| {
+            // Make sure to go in reverse order through the stack,'
+            // as the last item needs to be closed first.
+            remaining.iter().rev().fold(0, |acc, c| {
+                acc * 5 + insert_bracket_score(*c)
+            })
+        }))
+        .collect();
+    fix_missing_scores.sort_unstable();
+
+    println!("Fix missing brackets score: {:#10}", fix_missing_scores[fix_missing_scores.len() / 2]);
+}
