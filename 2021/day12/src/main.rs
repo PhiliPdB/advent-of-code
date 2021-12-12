8a3747bb4d188ae250fef4ef7299bc3b079aeb6e
PhiliPdB <phlpdbrn@gmail.com>
2021-12-12 12:14:20 +0100

    Speed up day 12
    But reduce flexibility as it can't handle bigger graphs now...

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -1,21 +1,40 @@
-use std::collections::{HashMap, VecDeque, HashSet};
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Debug, Clone, Copy)]
+pub struct BitSet(u32);
+
+impl BitSet {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    /// Check if the bit on the specified index is set
+    pub fn get(&self, index: u32) -> bool {
+        (self.0 >> index) & 1 == 1
+    }
+
+    /// Set bit on specified index
+    pub fn set(&mut self, index: u32) {
+        self.0 |= 1 << index;
+    }
+}
+
 
 #[derive(Debug)]
 pub struct Graph {
-    adjacency_list: Vec<Vec<i32>>,
+    adjacency_list: Vec<Vec<u32>>,
     big_cave: Vec<bool>,
-    start_vertex: i32,
-    end_vertex: i32,
+    start_vertex: u32,
+    end_vertex: u32,
 }
 
 impl Graph {
-
-    fn new(s: &str) -> Self {
+    pub fn new(s: &str) -> Self {
         let mut adjacency_list = Vec::new();
         let mut vertex_lookup = HashMap::new();
         let mut big_cave = Vec::new();
-        let mut start_vertex = -1;
-        let mut end_vertex = -1;
+        let mut start_vertex = 0;
+        let mut end_vertex = 0;
         let mut current_node_id = 0;
 
         for line in s.lines() {
@@ -53,18 +72,21 @@ impl Graph {
 
 fn paths(graph: &Graph, allow_visiting_twice: bool) -> i32 {
     let mut paths = 0;
-    let mut queue = VecDeque::from([(graph.start_vertex, HashSet::new(), HashSet::new())]);
+    let mut queue = VecDeque::from([(graph.start_vertex, BitSet::new(), false)]);
 
     while let Some((current, visited, visited_twice)) = queue.pop_front() {
         let mut new_visited = visited;
         let mut new_visited_twice = visited_twice;
-        if !graph.big_cave[current as usize] && !new_visited.insert(current) {
-            if allow_visiting_twice && new_visited_twice.len() < 1 {
-                new_visited_twice.insert(current);
+        // Check if we are allowed to visit this vertex
+        if !graph.big_cave[current as usize] && new_visited.get(current) {
+            if allow_visiting_twice && !new_visited_twice {
+                new_visited_twice = true;
             } else {
                 continue;
             }
         }
+        // Visit this vertex
+        new_visited.set(current);
 
         if current == graph.end_vertex {
             paths += 1;
@@ -73,7 +95,7 @@ fn paths(graph: &Graph, allow_visiting_twice: bool) -> i32 {
 
         for neighbour in &graph.adjacency_list[current as usize] {
             if *neighbour != graph.start_vertex {
-                queue.push_back((*neighbour, new_visited.clone(), new_visited_twice.clone()));
+                queue.push_back((*neighbour, new_visited, new_visited_twice));
             }
         }
     }
