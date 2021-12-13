ed9d96b5f11dcb6bb435a3c9c7179ebccc416739
PhiliPdB <phlpdbrn@gmail.com>
2021-12-13 18:40:41 +0100

    Reduce runtime to about 1s

--- 2020/day15/src/main.rs
+++ 2020/day15/src/main.rs
@@ -1,25 +1,22 @@
-use std::collections::HashMap;
-
-const NUMBER: usize = 30_000_000;
+const TOTAL_TURNS: u32 = 30_000_000;
 
 fn main() {
     let input = [2, 20, 0, 4, 1, 17];
 
-    let mut spoken: HashMap<_, _> = HashMap::from_iter(
-        input.iter().enumerate()
-            .map(|(i, n)| (*n, i + 1))
-    );
+    let mut spoken = vec![None; TOTAL_TURNS as usize];
+    for (i, n) in input.iter().enumerate() {
+        spoken[*n] = Some(i as u32);
+    }
 
-    let mut turn = input.len() + 1;
     let mut next_number = 0;
-    while turn < NUMBER {
-        if let Some(last_turn) = spoken.insert(next_number, turn) {
-            next_number = turn - last_turn;
+    for turn in (input.len() as u32 + 1)..TOTAL_TURNS {
+        if let Some(last_turn) = spoken[next_number] {
+            spoken[next_number] = Some(turn);
+            next_number = (turn - last_turn) as usize;
         } else {
+            spoken[next_number] = Some(turn);
             next_number = 0;
-        }
-
-        turn += 1;
+        };
     }
 
     println!("Number: {}", next_number);
