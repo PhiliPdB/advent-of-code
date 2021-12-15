fce2edce77bd37d09cab63a7e34f20f437e495c8
PhiliPdB <phlpdbrn@gmail.com>
2021-12-15 08:08:33 +0100

    Day 15 part 2

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -21,8 +21,21 @@ impl Ord for HeapItem {
 }
 
 
-fn add_item(heap: &mut BinaryHeap<HeapItem>, current_item: &HeapItem, input: &[Vec<u32>], risk_levels: &mut [Vec<u32>], x: usize, y: usize) {
-    let new_risk_level = current_item.risk_level + input[y][x];
+fn add_item(
+    heap: &mut BinaryHeap<HeapItem>,
+    current_item: &HeapItem,
+    input: &[Vec<u32>],
+    risk_levels: &mut [Vec<u32>],
+    x: usize, y: usize,
+    width: usize, height: usize
+) {
+    // Calculate risk level on this tile
+    let mut risk_on_new_item = input[y % height][x % width] + (y / height) as u32 + (x / width) as u32;
+    if risk_on_new_item > 9 {
+        risk_on_new_item -= 9;
+    }
+
+    let new_risk_level = current_item.risk_level + risk_on_new_item;
     if new_risk_level < risk_levels[y][x] {
         // Found a shorter way
         heap.push(HeapItem { position: (x, y), risk_level: new_risk_level });
@@ -40,43 +53,43 @@ fn main() {
     let height = input.len();
     let width = input[0].len();
 
-    let mut risk_levels = vec![vec![u32::MAX; width]; height];
+    // Perform Dijkstra
+
+    let mut risk_levels = vec![vec![u32::MAX; width * 5]; height * 5];
     let mut heap = BinaryHeap::new();
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         heap.push(HeapItem { node: (x, y), risk_level: input[y][x] });
-    //     }
-    // }
 
     risk_levels[0][0] = 0;
     heap.push(HeapItem { position: (0, 0), risk_level: 0 });
 
     while let Some(current_item) = heap.pop() {
-        if current_item.position == (width - 1, height - 1) {
+        // Stop when we reach our end location
+        if current_item.position == (5 * width - 1, 5 * height - 1) {
             break;
         }
 
         let pos_x = current_item.position.0;
         let pos_y = current_item.position.1;
+        // Already found a better path, so skip.
         if current_item.risk_level > risk_levels[pos_y][pos_x] {
             continue;
         }
 
         // Add new edges
         if pos_x > 0 {
-            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x - 1, pos_y);
+            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x - 1, pos_y, width, height);
         }
-        if pos_x < width - 1 {
-            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x + 1, pos_y);
+        if pos_x < 5 * width - 1 {
+            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x + 1, pos_y, width, height);
         }
 
         if pos_y > 0 {
-            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x, pos_y - 1);
+            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x, pos_y - 1, width, height);
         }
-        if pos_y < height - 1 {
-            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x, pos_y + 1);
+        if pos_y < 5 * height - 1 {
+            add_item(&mut heap, &current_item, &input, &mut risk_levels, pos_x, pos_y + 1, width, height);
         }
     }
 
-    println!("Path risk: {}", risk_levels[height - 1][width - 1]);
+    println!("[Part 1] Path risk: {:#4}", risk_levels[height - 1][width - 1]);
+    println!("[Part 2] Path risk: {:#4}", risk_levels[5 * height - 1][5 * width - 1]);
 }
