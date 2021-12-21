46239c37b72e6a6bcf74625d0e54f6410e4aeda5
PhiliPdB <phlpdbrn@gmail.com>
2021-12-21 08:15:09 +0100

    Day 21 part 2

--- 2021/day21/src/main.rs
+++ 2021/day21/src/main.rs
@@ -6,9 +6,9 @@
 const PLAYER1_STARTING: i32 = 5;
 const PLAYER2_STARTING: i32 = 6;
 
-struct Die(i32);
+struct DeterministicDie(i32);
 
-impl Die {
+impl DeterministicDie {
     fn roll(&mut self) -> i32 {
         let value = self.0;
         self.0 += 1;
@@ -19,14 +19,13 @@ impl Die {
     }
 }
 
-
-fn main() {
+fn play_deterministic() -> i32 {
     let mut player_positions = [PLAYER1_STARTING - 1, PLAYER2_STARTING - 1];
     let mut player_scores = [0, 0];
 
 
     let mut round = 0;
-    let mut die = Die(1);
+    let mut die = DeterministicDie(1);
     while player_scores[0] < 1000 && player_scores[1] < 1000 {
         let player = round % 2;
 
@@ -39,7 +38,38 @@ fn main() {
         round += 1;
     }
 
-    let score = (round as i32) * 3 * player_scores[0].min(player_scores[1]);
-    println!("{} {}", round * 3, player_scores[0].min(player_scores[1]));
-    println!("Part 1: {}", score);
+    (round as i32) * 3 * player_scores[0].min(player_scores[1])
+}
+
+fn play_dirac(positions: [i32; 2], scores: [i32; 2], turn: usize) -> [i64; 2] {
+    if scores[0] >= 21 {
+        return [1, 0];
+    } else if scores[1] >= 21 {
+        return [0, 1];
+    }
+
+    let player = turn % 2;
+    let mut p1_wins = 0;
+    let mut p2_wins = 0;
+
+    for (die_outcome, amount) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+        let mut new_position = positions;
+        let mut new_score = scores;
+        new_position[player] += die_outcome;
+        new_position[player] %= 10;
+        new_score[player] += new_position[player] + 1;
+
+        let [p1, p2] = play_dirac(new_position, new_score, turn + 1);
+        p1_wins += p1 * amount;
+        p2_wins += p2 * amount;
+    }
+
+    [p1_wins, p2_wins]
+}
+
+
+fn main() {
+    println!("Part 1: {}", play_deterministic());
+
+    println!("Part 2: {:?}", play_dirac([PLAYER1_STARTING - 1, PLAYER2_STARTING - 1], [0, 0], 0).iter().max().unwrap())
 }
