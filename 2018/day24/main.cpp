cf78f69b8c16d5152fec07531c521256ca9d3dcb
PhiliPdB <phlpdbrn@gmail.com>
2022-02-26 20:29:11 +0100

    Solve part 1 of day 24 of 2018

--- 2018/day24/main.cpp
+++ 2018/day24/main.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <array>
+#include <execution>
 #include <fstream>
+#include <numeric>
 #include <iostream>
 #include <optional>
 #include <regex>
@@ -31,6 +33,14 @@ public:
           m_weaknesses{std::move(weaknesses)}, m_immunities{std::move(immunities)} {
     }
 
+    Group(const Group& group) = default;
+
+
+    [[nodiscard]]
+    constexpr int units() const {
+        return m_units;
+    }
+
     [[nodiscard]]
     constexpr int effective_power() const {
         return m_units * m_attack_damage;
@@ -54,6 +64,21 @@ public:
             return 0;
         } else {
             return power;
+        }
+    }
+
+    void attack(const int power, const std::string& attack) {
+        m_units -= damage_taken(power, attack) / m_hit_points;
+        m_units = std::max(m_units, 0);
+    }
+
+    auto operator<=>(const Group& group) const {
+        if (const auto order{ effective_power() <=> group.effective_power() };
+            order == std::strong_ordering::equal
+        ) {
+            return m_initiative <=> group.m_initiative;
+        } else {
+            return order;
         }
     }
 };
@@ -82,7 +107,11 @@ std::vector<std::optional<size_t>> targets(const std::vector<Group>& attackers, 
     for (const auto& group : attackers) {
         choosing_order.emplace_back(choosing_order.size(), &group);
     }
-    std::ranges::sort(choosing_order, {}, [](auto& e) { return e.second->effective_power(); });
+    std::ranges::sort(
+        choosing_order,
+        [](const Group* a, const Group* b) { return *a > *b; },
+        [](const auto& e) { return e.second; }
+    );
 
     std::unordered_set<size_t> selected_targets{};
     for (const auto& [index, group] : choosing_order) {
@@ -101,7 +130,7 @@ std::vector<std::optional<size_t>> targets(const std::vector<Group>& attackers, 
                 || (
                     defenders[i].damage_taken(group->effective_power(), group->attack_type()) == defenders[*target].damage_taken(group->effective_power(), group->attack_type())
                     && defenders[i].effective_power() == defenders[*target].effective_power()
-                    && defenders[i].initiative() < defenders[*target].initiative()
+                    && defenders[i].initiative() > defenders[*target].initiative()
                     )
                 ) {
                 target = i;
@@ -117,10 +146,20 @@ std::vector<std::optional<size_t>> targets(const std::vector<Group>& attackers, 
     return targets;
 }
 
+void remove_dead_groups(std::vector<Group>& groups) {
+    for (auto it{ groups.begin() }; it != groups.end();) {
+        if (it->units() == 0) {
+            it = groups.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
 
 int main() {
     // Read input
-    std::ifstream file{ "test_input.txt" };
+    std::ifstream file{ "input.txt" };
     if (!file) {
         std::cerr << "Can't open file: input.txt\n";
         return EXIT_FAILURE;
@@ -184,8 +223,9 @@ int main() {
     }
     file.close();
 
+
     // Part 1
-    while (true) {
+    while (!immune_system.empty() && !infection.empty()) {
         // FIGHT!
 
         // Run target selection
@@ -193,11 +233,80 @@ int main() {
         std::vector infection_targets{ targets(infection, immune_system) };
 
         // Let them attack
-        
-        break;
+        std::vector<std::pair<size_t, const Group*>> immune_order;
+        immune_order.reserve(immune_system.size());
+        for (const auto& group : immune_system) {
+            immune_order.emplace_back(immune_order.size(), &group);
+        }
+        std::ranges::sort(immune_order, std::ranges::greater{}, [](const auto& e) { return e.second->initiative(); });
+
+        std::vector<std::pair<size_t, const Group*>> infection_order;
+        infection_order.reserve(infection.size());
+        for (const auto& group : infection) {
+            infection_order.emplace_back(infection_order.size(), &group);
+        }
+        std::ranges::sort(infection_order, std::ranges::greater{}, [](const auto& e) { return e.second->initiative(); });
+
+        size_t immune_index{ 0 };
+        size_t infection_index{ 0 };
+        while (immune_index < immune_order.size() || infection_index < infection_order.size()) {
+            const auto immune_initiative{
+                immune_index < immune_order.size()
+                    ? immune_order[immune_index].second->initiative()
+                    : -1
+            };
+            const auto infection_initiative{
+                infection_index < infection_order.size()
+                    ? infection_order[infection_index].second->initiative()
+                    : -1
+            };
+
+            if (immune_initiative > infection_initiative) {
+                if (const auto target{ immune_targets[immune_order[immune_index].first] }; target.has_value()) {
+                    infection[*target].attack(
+                        immune_order[immune_index].second->effective_power(),
+                        immune_order[immune_index].second->attack_type()
+                    );
+                }
+                ++immune_index;
+            } else {
+                if (const auto target{ infection_targets[infection_order[infection_index].first] }; target.has_value()) {
+                    immune_system[*target].attack(
+                        infection_order[infection_index].second->effective_power(),
+                        infection_order[infection_index].second->attack_type()
+                    );
+                }
+                ++infection_index;
+            }
+        }
+
+        // Remove dead groups
+        remove_dead_groups(immune_system);
+        remove_dead_groups(infection);
     }
 
-
+    int winning_army_units;
+    if (!immune_system.empty()) {
+        winning_army_units = std::transform_reduce(
+            std::execution::par_unseq,
+            immune_system.begin(), immune_system.end(), 
+            0,
+            std::plus(),
+            [](const auto& e) { return e.units(); }
+        );
+    } else {
+        winning_army_units = std::transform_reduce(
+            std::execution::par_unseq,
+            infection.begin(), infection.end(),
+            0,
+            std::plus(),
+            [](const auto& e) { return e.units(); }
+        );
+    }
+    std::cout << "Winning army units: " << winning_army_units << '\n';
 
     return EXIT_SUCCESS;
 }
+
+// Too High: 22680
+// Too Low: 20622
