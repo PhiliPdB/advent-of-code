0cc43b7d42b6ec74976ae8344a5c64ea0b031c49
PhiliPdB <phlpdbrn@gmail.com>
2022-10-22 15:39:08 +0200

    Solve day 9 of 2016

---
+++ 2016/day09/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string input = File.ReadAllLines("./input.txt")[0];
+
+long DecompressedLength(string s, bool recursive) {
+    long decompressedLength = 0;
+    var pos = 0;
+    while (pos < s.Length) {
+        if (s[pos] == '(') {
+            int closingOffset = s[pos..].Select((c, i) => (c, i)).First(p => p.c == ')').i;
+            string[] marker = s[(pos + 1)..(pos + closingOffset)].Split('x');
+            
+            int chars = int.Parse(marker[0]);
+            int times = int.Parse(marker[1]);
+            if (recursive) {
+                int start = pos + closingOffset + 1;
+                decompressedLength += DecompressedLength(s[start..(start + chars)], recursive) * times;
+            } else {
+                decompressedLength += chars * times;
+            }
+
+            pos += closingOffset + chars + 1;
+        } else {
+            decompressedLength++;
+            pos++;
+        }
+    }
+
+    return decompressedLength;
+}
+
+Console.WriteLine($"[Part 1] Decompressed length: {DecompressedLength(input, false)}");
+Console.WriteLine($"[Part 2] Decompressed length: {DecompressedLength(input, true)}");
