129330c31fff9920e9150bd384103ed188fa4e12
PhiliPdB <phlpdbrn@gmail.com>
2022-10-22 13:31:40 +0200

    Solve day 6 of 2016

---
+++ 2016/day06/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string[] input = File.ReadAllLines("./input.txt");
+
+List<char[]> columns = new();
+for (var i = 0; i < input[0].Length; i++) {
+    columns.Add(input.Select(l => l[i]).ToArray());
+}
+
+
+// Part 1
+string part1ErrorCorrectedMessage = string.Concat(
+    columns.Select(
+        column => column.GroupBy(c => c)
+            .OrderByDescending(g => g.Count())
+            .First().Key
+    )
+);
+Console.WriteLine($"[Part 1] Message: {part1ErrorCorrectedMessage}");
+
+// Part 2
+string part2ErrorCorrectedMessage = string.Concat(
+    columns.Select(
+        column => column.GroupBy(c => c)
+            .OrderBy(g => g.Count())
+            .First().Key
+    )
+);
+Console.WriteLine($"[Part 2] Message: {part2ErrorCorrectedMessage}");
