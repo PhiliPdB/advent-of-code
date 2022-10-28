3b540b0b4a3cbb38f059df47f036c2aa8ec1589e
PhiliPdB <phlpdbrn@gmail.com>
2022-10-28 17:18:40 +0200

    Some refactoring of day 11

---
+++ 2016/day11/Item.cs
@@ -0,0 +1,44 @@
+﻿namespace day11;
+
+internal enum Type {
+    Chip, Generator
+}
+
+internal enum Rock {
+    Curium,
+    Elerium,
+    Dilithium,
+    Plutionium,
+    Ruthenium,
+    Strontium,
+    Thulium,
+}
+
+internal struct Item: IEquatable<Item>, IComparable<Item> {
+    public Type type;
+    public Rock rock;
+
+    public Item(Type type, Rock rock) {
+        this.type = type;
+        this.rock = rock;
+    }
+
+    public bool Equals(Item other) {
+        return this.type == other.type && this.rock == other.rock;
+    }
+
+    public override bool Equals(object? obj) {
+        return obj is Item other && this.Equals(other);
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine((int) this.type, this.rock);
+    }
+
+    public int CompareTo(Item other) {
+        int typeComparison = this.type.CompareTo(other.type);
+        if (typeComparison != 0) return typeComparison;
+        
+        return this.rock.CompareTo(other.rock);
+    }
+}
---
+++ 2016/day11/Priority.cs
@@ -0,0 +1,15 @@
+﻿namespace day11;
+
+internal struct Priority: IComparable<Priority> {
+    public int moves;
+    public int topFloorItems;
+
+    public int CompareTo(Priority other) {
+        int movesComparison = this.moves.CompareTo(other.moves);
+        if (movesComparison != 0) {
+            return movesComparison;
+        }
+
+        return other.topFloorItems.CompareTo(this.topFloorItems);
+    }
+}
