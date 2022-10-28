2ed50979bd3a897c0a379fcee1916380b5f9373f
PhiliPdB <phlpdbrn@gmail.com>
2022-10-28 08:40:58 +0200

    Solve day 12 of 2016

---
+++ 2016/day12/Instruction.cs
@@ -0,0 +1,69 @@
+﻿namespace day12;
+
+internal abstract class Instruction {
+    
+    public abstract void Execute(int[] registers, ref int programCounter);
+
+}
+
+internal class Copy : Instruction {
+    private int valX;
+    private int valY;
+    private bool xIsRegister;
+
+    public Copy(int valX, int valY, bool xIsRegister) {
+        this.valX = valX;
+        this.valY = valY;
+        this.xIsRegister = xIsRegister;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter) {
+        registers[this.valY] = this.xIsRegister
+            ? registers[this.valX]
+            : this.valX;
+    }
+}
+
+internal class Increase : Instruction {
+    private int register;
+
+    public Increase(int register) {
+        this.register = register;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter) {
+        registers[this.register]++;
+    }
+}
+
+internal class Decrease : Instruction {
+    private int register;
+
+    public Decrease(int register) {
+        this.register = register;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter) {
+        registers[this.register]--;
+    }
+}
+
+internal class JumpNotZero : Instruction {
+    private int register;
+    private int offset;
+    private bool isRegister;
+
+    public JumpNotZero(int register, int offset, bool isRegister) {
+        this.register = register;
+        this.offset = offset;
+        this.isRegister = isRegister;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter) {
+        if ((this.isRegister && registers[this.register] != 0)
+            || (!this.isRegister && this.register != 0)
+        ) {
+            programCounter += this.offset - 1;
+        }
+    }
+}
---
+++ 2016/day12/Program.cs
@@ -0,0 +1,54 @@
+﻿using day12;
+
+Instruction[] instructions = File.ReadAllLines("./input.txt")
+    .Select<string, Instruction>(line => {
+        string[] splitted = line.Split(' ');
+        switch (splitted[0]) {
+            case "cpy": {
+                bool xIsReg = !int.TryParse(splitted[1], out int xReg);
+                int yReg = splitted[2][0] - 'a';
+                if (xIsReg) {
+                    xReg = splitted[1][0] - 'a';
+                }
+                return new Copy(xReg, yReg, xIsReg);
+            }
+            case "inc": {
+                int register = splitted[1][0] - 'a';
+                return new Increase(register);
+            }
+            case "dec": {
+                int register = splitted[1][0] - 'a';
+                return new Decrease(register);
+            }
+            case "jnz": {
+                bool isReg = !int.TryParse(splitted[1], out int reg);
+                if (isReg) {
+                    reg = splitted[1][0] - 'a';
+                }
+                int offset = int.Parse(splitted[2]);
+                return new JumpNotZero(reg, offset, isReg);
+            }
+            default:
+                throw new Exception("Unable to parse instruction");
+        }
+    })
+    .ToArray();
+
+
+void ExecuteProgram(int[] registers) {
+    var programCounter = 0;
+    while (programCounter >= 0 && programCounter < instructions.Length) {
+        instructions[programCounter].Execute(registers, ref programCounter);
+        programCounter++;
+    }
+}
+
+// Part 1
+int[] registers = { 0, 0, 0, 0 };
+ExecuteProgram(registers);
+Console.WriteLine($"[Part 1] Value in register 'a': {registers[0]}");
+
+// Part 2
+registers = new[] { 0, 0, 1, 0 };
+ExecuteProgram(registers);
+Console.WriteLine($"[Part 2] Value in register 'a': {registers[0]}");
