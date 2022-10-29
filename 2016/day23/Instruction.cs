c8b6899035f6407ccb8e055a12a3906eb9418bd9
PhiliPdB <phlpdbrn@gmail.com>
2022-10-29 20:33:35 +0200

    Solve day 23 of 2016

---
+++ 2016/day23/Instruction.cs
@@ -0,0 +1,103 @@
+﻿namespace day23;
+
+internal abstract class Instruction {
+    
+    public abstract void Execute(int[] registers, ref int programCounter, Instruction[] instructions);
+
+}
+
+internal class Copy : Instruction {
+    public int valX { get; init; }
+    public int valY { get; init; }
+    public bool xIsRegister { get; init; }
+    public bool yIsRegister { get; init; }
+
+    public Copy(int valX, int valY, bool xIsRegister, bool yIsRegister) {
+        this.valX = valX;
+        this.valY = valY;
+        this.xIsRegister = xIsRegister;
+        this.yIsRegister = yIsRegister;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter, Instruction[] instructions) {
+        if (!this.yIsRegister) {
+            return;
+        }
+
+        registers[this.valY] = this.xIsRegister
+            ? registers[this.valX]
+            : this.valX;
+    }
+}
+
+internal class Increase : Instruction {
+    public int register { get; init; }
+
+    public Increase(int register) {
+        this.register = register;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter, Instruction[] instructions) {
+        registers[this.register]++;
+    }
+}
+
+internal class Decrease : Instruction {
+    public int register { get; init; }
+    public bool isRegister { get; init; }
+
+    public Decrease(int register) {
+        this.register = register;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter, Instruction[] instructions) {
+        registers[this.register]--;
+    }
+}
+
+internal class JumpNotZero : Instruction {
+    public int register { get; init; }
+    public int offset { get; init; }
+    public bool isRegister { get; init; }
+    public bool offsetIsRegister { get; init; }
+
+    public JumpNotZero(int register, int offset, bool isRegister, bool offsetIsRegister) {
+        this.register = register;
+        this.offset = offset;
+        this.isRegister = isRegister;
+        this.offsetIsRegister = offsetIsRegister;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter, Instruction[] instructions) {
+        if ((this.isRegister && registers[this.register] != 0)
+            || (!this.isRegister && this.register != 0)
+        ) {
+            int offset = this.offsetIsRegister ? registers[this.offset] : this.offset;
+            programCounter += offset - 1;
+        }
+    }
+}
+
+internal class Toggle : Instruction {
+    public int register { get; init; }
+
+    public Toggle(int register) {
+        this.register = register;
+    }
+
+    public override void Execute(int[] registers, ref int programCounter, Instruction[] instructions) {
+        int index = programCounter + registers[this.register];
+        if (index < 0 || index >= instructions.Length) {
+            return;
+        }
+
+        instructions[index] = instructions[index] switch {
+            Copy copy => new JumpNotZero(copy.valX, copy.valY, copy.xIsRegister, copy.yIsRegister),
+            Decrease decrease => new Increase(decrease.register),
+            Increase increase => new Decrease(increase.register),
+            JumpNotZero jumpNotZero => new Copy(jumpNotZero.register, jumpNotZero.offset, jumpNotZero.isRegister, jumpNotZero.offsetIsRegister),
+            Toggle toggle => new Increase(toggle.register),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
---
+++ 2016/day23/Program.cs
@@ -0,0 +1,66 @@
+﻿using day23;
+
+static Instruction[] ReadInstructions() {
+    return File.ReadAllLines("./input.txt")
+        .Select<string, Instruction>(line => {
+            string[] splitted = line.Split(' ');
+            switch (splitted[0]) {
+                case "cpy": {
+                    bool xIsReg = !int.TryParse(splitted[1], out int xReg);
+                    int yReg = splitted[2][0] - 'a';
+                    if (xIsReg) {
+                        xReg = splitted[1][0] - 'a';
+                    }
+
+                    return new Copy(xReg, yReg, xIsReg, true);
+                }
+                case "inc": {
+                    int register = splitted[1][0] - 'a';
+                    return new Increase(register);
+                }
+                case "dec": {
+                    int register = splitted[1][0] - 'a';
+                    return new Decrease(register);
+                }
+                case "jnz": {
+                    bool isReg = !int.TryParse(splitted[1], out int reg);
+                    if (isReg) {
+                        reg = splitted[1][0] - 'a';
+                    }
+
+                    bool offsetIsReg = !int.TryParse(splitted[2], out int offset);
+                    if (offsetIsReg) {
+                        offset = splitted[2][0] - 'a';
+                    }
+
+                    return new JumpNotZero(reg, offset, isReg, offsetIsReg);
+                }
+                case "tgl": {
+                    int reg = splitted[1][0] - 'a';
+                    return new Toggle(reg);
+                }
+                default:
+                    throw new Exception("Unable to parse instruction");
+            }
+        })
+        .ToArray();
+}
+
+Instruction[] instructions = ReadInstructions();
+
+void ExecuteProgram(int[] registers, int programCounter = 0) {
+    while (programCounter >= 0 && programCounter < instructions.Length) {
+        instructions[programCounter].Execute(registers, ref programCounter, instructions);
+        programCounter++;
+    }
+}
+
+int[] registers = { 7, 0, 0, 0 };
+ExecuteProgram(registers);
+Console.WriteLine($"[Part 1] Value for safe: {registers[0]}");
+
+// Part 2
+instructions = ReadInstructions();
+registers = new [] { 12 * 11, 11, 0, 0 };
+ExecuteProgram(registers, 10);
+Console.WriteLine($"[Part 2] Value for safe: {registers[0]}");
