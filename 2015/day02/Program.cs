52051b4a84db1c76bb2711ee08f84f4dda668b2d
PhiliPdB <phlpdbrn@gmail.com>
2022-10-31 20:24:41 +0100

    Solve days 1-5 of 2015

---
+++ 2015/day02/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var sizes = File.ReadAllLines("./input.txt")
+    .Select(line => line.Split('x').Select(int.Parse).ToArray());
+
+var toOrder = sizes
+    .Sum(size => {
+        int side1 = size[0] * size[1];
+        int side2 = size[1] * size[2];
+        int side3 = size[0] * size[2];
+
+        return 2 * side1 + 2 * side2 + 2 * side3 + Math.Min(Math.Min(side1, side2), side3);
+    });
+Console.WriteLine($"[Part 1] Square feet of wrapping paper to order: {toOrder}");
+
+var ribbonLength = sizes
+    .Sum(size => {
+        int biggest = size.Max();
+
+        int side1;
+        int side2;
+        if (size[0] == biggest) {
+            side1 = size[1];
+            side2 = size[2];
+        } else if (size[1] == biggest) {
+            side1 = size[0];
+            side2 = size[2];
+        } else {
+            side1 = size[0];
+            side2 = size[1];
+        }
+
+        return side1 * 2 + side2 * 2 + size[0] * size[1] * size[2];
+    });
+Console.WriteLine($"[Part 2] Feet of ribbon to order: {ribbonLength}");
---
+++ 2015/day04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+const string secret = "yzbqklnj";
+
+using var md5Hash = MD5.Create();
+
+int part1Answer = -1;
+var number = 1;
+while (true) {
+    byte[] byteHash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(secret + number));
+    string s = Convert.ToHexString(byteHash);
+
+    if (s.StartsWith("00000")) {
+        if (part1Answer == -1) {
+            part1Answer = number;
+        }
+        if (s.StartsWith("000000")) {
+            break;
+        }
+    }
+    number++;
+}
+
+Console.WriteLine($"[Part 1] Answer: {part1Answer}");
+Console.WriteLine($"[Part 2] Answer: {number}");
