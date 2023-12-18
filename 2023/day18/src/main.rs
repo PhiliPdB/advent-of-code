d5db4d2186f3f7a61e6b310edbf9b12970e17f32
PhiliPdB <phlpdbrn@gmail.com>
2023-12-18 07:18:25 +0100

    Day 18 of 2023

---
+++ 2023/day18/src/main.rs
@@ -0,0 +1,111 @@
+use std::str::FromStr;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up, Down, Left, Right,
+}
+
+impl Direction {
+    fn from_char(c: char) -> Self {
+        match c {
+            'U' => Self::Up,
+            'D' => Self::Down,
+            'L' => Self::Left,
+            'R' => Self::Right,
+            _ => panic!("Invalid direction: {c}"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Instruction {
+    direction: Direction,
+    distance: i64,
+}
+
+impl Instruction {
+    fn parse_from_color(s: &str) -> Self {
+        let parts: Vec<_> = s.split_whitespace().collect();
+
+        let distance = parts[2][2..7].chars()
+            .map(|c| c.to_digit(16).unwrap() as i64)
+            .fold(0, |acc, c| 16 * acc + c);
+
+        let direction = match parts[2][7..].chars().next().unwrap() {
+            '0' => Direction::Right,
+            '1' => Direction::Down,
+            '2' => Direction::Left,
+            '3' => Direction::Up,
+            d => panic!("Invalid direction: {d}"),
+        };
+
+        Self { direction, distance }
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split_whitespace().collect();
+        let direction = Direction::from_char(parts[0].chars().next().unwrap());
+
+        let distance = parts[1].parse().unwrap();
+
+        Ok(Self { direction, distance })
+    }
+}
+
+fn calculate_volume(instructions: &[Instruction]) -> i64 {
+    let (mut current_x, mut current_y) = (1, 1);
+    let mut points = Vec::with_capacity(instructions.len());
+    points.push((current_x, current_y));
+
+    let mut perimeter_size = 0;
+    for Instruction { direction, distance } in instructions {
+        match direction {
+            Direction::Up => {
+                current_y -= distance;
+            },
+            Direction::Down => {
+                current_y += distance;
+            },
+            Direction::Left => {
+                current_x -= distance;
+            },
+            Direction::Right => {
+                current_x += distance;
+            },
+        }
+
+        points.push((current_x, current_y));
+        perimeter_size += distance;
+    }
+
+    let mut volume = 0;
+    for p in points.windows(2) {
+        let p1 = p[0];
+        let p2 = p[1];
+
+        volume += (p1.0 * p2.1) as i64 - (p2.0 * p1.1) as i64;
+    }
+
+    volume / 2 + perimeter_size / 2 + 1
+}
+
+
+fn main() {
+    let part1_instructions: Vec<_> = include_str!("../input.txt")
+        .lines()
+        .map(|l| Instruction::from_str(l).unwrap())
+        .collect();
+    println!("[Part 1] Volume: {}", calculate_volume(&part1_instructions));
+
+
+    let part2_instructions: Vec<_> = include_str!("../input.txt")
+        .lines()
+        .map(|l| Instruction::parse_from_color(l))
+        .collect();
+    println!("[Part 2] Volume: {}", calculate_volume(&part2_instructions));
+}
