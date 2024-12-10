a238d095070a7457cbdb8630b955fdac4d64e692
PhiliPdB <phlpdbrn@gmail.com>
2024-12-10 10:12:30 +0100

    Improve readability and error checking

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -1,6 +1,8 @@
 use std::str::FromStr;
 use std::collections::HashSet;
 
+use itertools::iproduct;
+
 
 #[derive(Debug)]
 struct Map {
@@ -8,25 +10,19 @@ struct Map {
 }
 
 impl Map {
-    pub fn trailheads<const DISTINCT: bool>(&self) -> Vec<((usize, usize), u32)> {
-        self.map.iter()
-            .enumerate()
-            .flat_map(|(y, row)| {
-                row.iter()
-                    .enumerate()
-                    .filter_map(move |(x, _)| {
-                        let num_trails = self.num_trails::<DISTINCT>((x, y));
-                        if num_trails > 0 {
-                            Some(((x, y), num_trails))
-                        } else {
-                            None
-                        }
-                    })
+    pub fn trailheads<const COUNT_DISTINCT: bool>(&self) -> impl Iterator<Item = ((usize, usize), u32)> + '_ {
+        iproduct!(0..self.map[0].len(), 0..self.map.len())
+            .filter_map(|(x, y)| {
+                let num_trails = self.num_trails::<COUNT_DISTINCT>((x, y));
+                if num_trails > 0 {
+                    Some(((x, y), num_trails))
+                } else {
+                    None
+                }
             })
-            .collect()
     }
 
-    fn num_trails<const DISTINCT: bool>(&self, (start_x, start_y): (usize, usize)) -> u32 {
+    fn num_trails<const COUNT_DISTINCT: bool>(&self, (start_x, start_y): (usize, usize)) -> u32 {
         if self.map[start_y][start_x] != 0 {
             return 0;
         }
@@ -36,7 +32,7 @@ impl Map {
         let mut visited = HashSet::new();
         while let Some(((x, y), h)) = queue.pop() {
             if h == 9 {
-                if DISTINCT || visited.insert((x, y)) {
+                if COUNT_DISTINCT || visited.insert((x, y)) {
                     num_trails += 1;
                 }
                 continue;
@@ -67,10 +63,10 @@ impl FromStr for Map {
         let map = s.lines()
             .map(|line| {
                 line.chars()
-                    .map(|c| c.to_digit(10).unwrap())
-                    .collect()
+                    .map(|c| c.to_digit(10).ok_or("Invalid digit in input"))
+                    .collect::<Result<Vec<_>,_>>()
             })
-            .collect();
+            .collect::<Result<Vec<_>,_>>()?;
 
         Ok(Map { map })
     }
@@ -82,14 +78,12 @@ fn main() {
 
 
     let part1_trailhead_sum: u32 = topographic_map.trailheads::<false>()
-        .into_iter()
         .map(|(_, num_trails)| num_trails)
         .sum();
     println!("[Part 1] Sum of trailheads: {part1_trailhead_sum:4}");
 
 
     let part2_trailhead_sum: u32 = topographic_map.trailheads::<true>()
-        .into_iter()
         .map(|(_, num_trails)| num_trails)
         .sum();
     println!("[Part 2] Sum of trailheads: {part2_trailhead_sum:4}");
