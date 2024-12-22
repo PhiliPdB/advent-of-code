d2d4161a7055fa2433b853129eaec623ce126555
PhiliPdB <phlpdbrn@gmail.com>
2024-12-22 12:03:04 +0100

    Runtime optimisation by removing HashMap and HashSet

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -1,7 +1,6 @@
-use hashbrown::{HashMap, HashSet};
-
-
 const SECRET_NUMBERS: u32 = 2000;
+const MAX_KEYS: usize = 19_usize.pow(4);
+
 
 const fn mix(a: u64, b: u64) -> u64 {
     a ^ b
@@ -24,6 +23,24 @@ const fn next_number(secret_number: u64) -> u64 {
     s
 }
 
+
+#[inline(always)]
+fn to_int(digits: &[i32]) -> usize {
+    digits.iter()
+        .fold(0, |acc, &digit| acc * 19 + (digit + 9) as usize)
+}
+
+fn to_digits(mut n: usize) -> Vec<i32> {
+    let mut digits = Vec::new();
+    while n > 0 {
+        digits.push((n % 19) as i32 - 9);
+        n /= 19;
+    }
+    digits.reverse();
+    digits
+}
+
+
 fn main() {
     let secret_numbers: Vec<_> = include_str!("../input.txt")
         .lines()
@@ -31,12 +48,13 @@ fn main() {
         .collect();
 
     // Keep track of total profit per sequence
-    let mut sequence_profit_map: HashMap<[i32; 4], u64> = HashMap::new();
+    let mut sequence_profit_map = vec![0; MAX_KEYS];
+    // Keep track of which sequences we have seen in an iteration (as only the first occurrence counts)
+    let mut seen = vec![false; MAX_KEYS];
 
     let mut part1_sum = 0;
     for secret_number in &secret_numbers {
         let mut result = *secret_number;
-        let mut seen = HashSet::new();
 
         let mut history = [0; 4];
         for i in 0..SECRET_NUMBERS {
@@ -47,9 +65,11 @@ fn main() {
             if i >= 3 {
                 history[3] = price_diff;
 
-                if seen.insert(history) {
-                    let current_profit = sequence_profit_map.entry(history).or_default();
-                    *current_profit += price;
+                let key = to_int(&history);
+                let is_seen = unsafe { seen.get_unchecked_mut(key) };
+                if !*is_seen {
+                    sequence_profit_map[key] += price;
+                    *is_seen = true;
                 }
 
                 history.rotate_left(1);
@@ -59,14 +79,22 @@ fn main() {
             result = next_result;
         }
 
+        // Add the resulting number to the part 1 answer
         part1_sum += result;
+
+        // Reset seen array (this way we don't need to create it for every number)
+        for s in seen.iter_mut() {
+            *s = false;
+        }
     }
 
     println!("[Part 1] Sum of 2000th secret number: {part1_sum}");
 
 
-    let (best_sequence, profit) = sequence_profit_map.iter()
-        .max_by_key(|(_, &profit)| profit)
+    let (best_sequence, profit) = sequence_profit_map.into_iter()
+        .enumerate()
+        .max_by_key(|(_, profit)| *profit)
         .unwrap();
+    let best_sequence = to_digits(best_sequence);
     println!("[Part 2] Max profit of {profit} with sequence {best_sequence:?}");
 }
