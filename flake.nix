04c57bdf1bcc6b58c99675294daa9c94d98a1cf2
PhiliPdB <phlpdbrn@gmail.com>
2025-10-07 22:31:01 +0200

    Update flake and add cargo asm

--- flake.nix
+++ flake.nix
@@ -9,73 +9,93 @@
     };
   };
 
-  outputs = { flake-parts, nixpkgs, rust-overlay, ... }@inputs:
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = { system, pkgs, ... }: {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
+      perSystem =
+        { system, pkgs, ... }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
 
-          config.permittedInsecurePackages = [
-            "dotnet-sdk-6.0.428" # Year 2016 is written with dotnet 6
-            "dotnet-sdk-7.0.410" # Year 2015 is written with dotnet 7
-          ];
-        };
-
-        devShells =
-          let
-            globalPackages = with pkgs; [
-              # Hyperfine for performance benchmarking
-              hyperfine
+            config.permittedInsecurePackages = [
+              "dotnet-sdk-6.0.428" # Year 2016 is written with dotnet 6
+              "dotnet-sdk-7.0.410" # Year 2015 is written with dotnet 7
             ];
-          in {
-            dotnet = pkgs.mkShell rec {
-              name = "AoC-dotnet";
-              dotnetPkg = (with pkgs.dotnetCorePackages; combinePackages [
-                sdk_6_0
-                sdk_7_0
-              ]);
-
-              dependencies = with pkgs; [
-                zlib
-                zlib.dev
-                icu
-                openssl
-
-                dotnetPkg
-              ];
-
-              NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([
-                pkgs.stdenv.cc.cc
-              ] ++ dependencies);
-              NIX_LD = "${pkgs.stdenv.cc.libc_bin}/bin/ld.so";
-
-              nativeBuildInputs = dependencies;
-
-              packages = globalPackages;
-
-              shellHook = ''
-                DOTNET_ROOT="${dotnetPkg}";
-              '';
-            };
-
-            rust = pkgs.mkShell {
-              name = "AoC-rust";
-
-              buildInputs = with pkgs; [
-                (rust-bin.stable.latest.default.override {
-                  extensions = [
-                    "clippy"
-                    "rust-analyzer"
-                  ];
-                })
-              ];
-
-              packages = globalPackages;
-            };
           };
-      };
+
+          devShells =
+            let
+              globalPackages = with pkgs; [
+                # Hyperfine for performance benchmarking
+                hyperfine
+              ];
+            in
+            {
+              dotnet = pkgs.mkShell rec {
+                name = "AoC-dotnet";
+                dotnetPkg = (
+                  with pkgs.dotnetCorePackages;
+                  combinePackages [
+                    sdk_6_0
+                    sdk_7_0
+                  ]
+                );
+
+                dependencies = with pkgs; [
+                  zlib
+                  zlib.dev
+                  icu
+                  openssl
+
+                  dotnetPkg
+                ];
+
+                NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+                  [
+                    pkgs.stdenv.cc.cc
+                  ]
+                  ++ dependencies
+                );
+                NIX_LD = "${pkgs.stdenv.cc.libc_bin}/bin/ld.so";
+
+                nativeBuildInputs = dependencies;
+
+                packages = globalPackages;
+
+                shellHook = ''
+                  DOTNET_ROOT="${dotnetPkg}";
+                '';
+              };
+
+              rust = pkgs.mkShell {
+                name = "AoC-rust";
+
+                buildInputs = with pkgs; [
+                  (rust-bin.stable.latest.default.override {
+                    extensions = [
+                      "clippy"
+                      "rust-analyzer"
+                    ];
+                  })
+                ];
+
+                packages =
+                  with pkgs;
+                  [
+                    cargo-show-asm
+                  ]
+                  ++ globalPackages;
+              };
+            };
+        };
     };
 }
